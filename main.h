25f5f57c6212c882d224888196a7fa96d16dd2f8
conkobar <connorkleine@gmail.com>
2022-07-25 13:41:54 -0500

    compiler error for newline at eof

--- main.h
+++ main.h
@@ -1,18 +1,18 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <wait.h>
 #include <sys/stat.h>
 
 int _strlen(char *s);
 int _putchar(char c);
-char **getav(char *);
-void print_array(char **av);
-int exe(char **cmd);
+char *prompt(void);
+char **split_string(char *str);
+int execute(char **cmd);
 
 #endif /* MAIN_H */
